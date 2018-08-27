849a9ead2cb7c6341e17067005e80f52cb9e1a02
Ming-Yuan Lu <ming-yuanlu@MBP-FVFVP117HV2D.local>
2018-08-26 22:18:57 -0500

    Add prediction-validation.py

---
+++ src/prediction-validation.py
@@ -0,0 +1,23 @@
+#!/bin/python
+
+import sys
+import numpy as np
+
+#arg 1: window file
+#arg 2: true value file
+#arg 3: predicted value file
+#arg 4: output file
+
+if (len(sys.argv) != 5):
+    print("Expected 4 arguments. Now have: %d") % (len(sys.argv))
+    return -1
+
+windowSizeFile = sys.argv[1]
+trueData       = sys.argv[2]
+predictedData  = sys.argv[3]
+outputFile     = sys.argv[4]
+
+window = myModule.readWindowSize(windowSizeFile)
+#need to check that there are at least "window" number of hours
+data = Data(trueData, predictedData)
+myModule.printError(window, data, ouputFile)
